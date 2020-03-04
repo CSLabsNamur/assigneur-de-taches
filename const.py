b00cba5dd1315b77da4c287fa32590f6adc003d0
François Romain <francoisromain4@gmail.com>
2020-03-04 15:21:06 +0100

    add const + meme nombre de tache par personne

---
+++ const.py
@@ -0,0 +1,4 @@
+f_path_members = "membres.txt"
+f_path_tasks = "task.txt"
+f_path_periods = "periods.txt"
+f_path_output = "output.txt"
--- main.py
+++ main.py
@@ -1,37 +1,42 @@
 import random
+import const
 
-f_path_members = "membres.txt"
-f_path_tasks = "task.txt"
-f_path_periods = "periods.txt"
-f_path_output = "output.txt"
 
 def create_output(attributed_tasks):
     """ Create the output file to visualize the information """
-    #todo : L’application génère un pdf avec le programme du week-end : pour chaque moment et pour chaque tâche, qui doit la réaliser
-    with open(f_path_output, "w") as file_output:
+    # todo : L’application génère un pdf avec le programme du week-end : pour chaque moment et pour chaque tâche, qui doit la réaliser
+    with open(const.f_path_output, "w") as file_output:
         for period in attributed_tasks:
-            file_output.write("==================== " + period["period"].upper() + " ====================\n")
+            file_output.write("==================== " +
+                              period["period"].upper() + " ====================\n")
             for task in period["tasks"]:
                 file_output.write("Task : " + task["name"] + "\n")
-                file_output.write("Members : " + ", ".join([str(x) for x in task["members"]]) + "\n\n")
-        
+                file_output.write(
+                    "Members : " + ", ".join([str(x) for x in task["members"]]) + "\n\n")
+
         file_output.close()
 
 
 def get_members():
     """ Returns the member list from the file """
-    with open(f_path_members, "r") as data_members:
-        members = [x.strip() for x in data_members.readlines()]
+    with open(const.f_path_members, "r") as data_members:
+        temp_members = [x.strip() for x in data_members.readlines()]
+        members = []
+        for member in temp_members:
+            members.append({
+                "name": member,
+                "tasks": []
+            })
         data_members.close()
         return members
 
 
 def get_tasks():
     """ Returns the task list from the file """
-    with open(f_path_tasks, "r") as data_tasks:
+    with open(const.f_path_tasks, "r") as data_tasks:
         tasks = []
+        line = [x.strip() for x in data_tasks.readline().split()]
         while True:
-            line = [x.strip() for x in data_tasks.readline().split()]
             if not line:
                 break
             tasks.append({
@@ -39,6 +44,7 @@ def get_tasks():
                 "person": line[1],
                 "occurence": [x.strip() for x in data_tasks.readline().split(", ")]
             })
+            line = [x.strip() for x in data_tasks.readline().split()]
         data_tasks.close()
         return tasks
 
@@ -46,28 +52,27 @@ def get_tasks():
 def init_periods_info():
     """ Init the periods_info """
     periods_info = []
-    with open(f_path_periods, "r") as data_periods:
-        while True:
-            line = data_periods.readline().strip()
-            if not line:
-                break
+    with open(const.f_path_periods, "r") as data_periods:
+        line = data_periods.readline().strip()
+        while line:
             periods_info.append({
                 "name": line,
                 "busy_members": [x.strip() for x in data_periods.readline().split(", ")]
             })
+            line = data_periods.readline().strip()
         data_periods.close()
         return periods_info
 
 
 def assign_tasks():
     """ Assign tasks to the members """
-    #todo : Essayer que chaque personne fasse plus ou moins le même nombre de tâches
-    #todo : Essayer qu’une personne ne fasse pas plusieurs tâches d’affilée
-    #todo : Essayer d’éviter un maximum qu’une personne fasse plusieurs fois la même tâche
+    # todo : Essayer qu’une personne ne fasse pas plusieurs tâches d’affilée
+    # todo : Essayer d’éviter un maximum qu’une personne fasse plusieurs fois la même tâche
     members = get_members()
     tasks = get_tasks()
     periods_info = init_periods_info()
     attributed_tasks = []
+    nbTasks = 0
 
     for period in periods_info:
         temp_task = []
@@ -77,17 +82,20 @@ def assign_tasks():
 
                 i = 0
                 while i < int(task["person"]):
-                    while True:
-                        member = random.choice(members)    
-                        if member not in period["busy_members"]:
-                            break
+                    member = random.choice(members)
+                    while member in period["busy_members"] or int(len(member["tasks"]) > nbTasks/int(len(members))):
+                        member = random.choice(members)
+
+                    nbTasks += 1
                     period["busy_members"].append(member)
-                    temp_members.append(member)
+                    members[members.index(member)]["tasks"].append(
+                        task["name"])
+                    temp_members.append(member["name"])
                     i += 1
 
                 temp_task.append({
-                    "name" : task["name"],
-                    "members" : temp_members
+                    "name": task["name"],
+                    "members": temp_members
                 })
         attributed_tasks.append({
             "period": period["name"],
