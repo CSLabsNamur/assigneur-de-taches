48bb288e886e04d9002cbf2e89a3b1a63e6fbea4
Maxime béver <max.bever98@gmail.com>
2020-03-16 17:55:04 +0100

    Algo to choose member

--- const.py
+++ const.py
@@ -1,4 +1,3 @@
 f_path_members = "membres.txt"
 f_path_tasks = "task.txt"
-f_path_periods = "periods.txt"
 f_path_output = "output.txt"
--- main.py
+++ main.py
@@ -25,7 +25,8 @@ def get_members():
         for member in temp_members:
             members.append({
                 "name": member,
-                "tasks": []
+                "tasks": [],
+                "occurence":[]
             })
         data_members.close()
         return members
@@ -49,35 +50,56 @@ def get_tasks_and_occurences():
         data_tasks.close()
         return (tasks,list(dict.fromkeys(occurences)))
 
+def choose_member(task_name,period,members,member_periods_prec):
+    members_available = members.copy()
+    sum_nbTask = 0
+    # Supprime les membres qui sont déja affectés à une tache pour la période
+    for member in members:
+        sum_nbTask += len(member["tasks"])
+        if(period in member["occurence"]):
+            members_available.remove(member)
+    # Supprime les membres qui sont au dessus de la moyenne de taches affectées
+    mean_nbTask = sum_nbTask / len(members)
+    members_available_temp = members_available.copy()
+    for member_available in members_available_temp:
+        if((len(member_available["tasks"]) > mean_nbTask)):
+            members_available.remove(member_available)
+    # Evite;un maximum qu’une personne fasse plusieurs tâches d’affilée si il y a assez de membres
+    # Et essayer d’éviter qu’une personne fasse plusieurs fois la même tâche
+    members_available_not_in_row = members_available.copy()
+    for member_not_in_row in members_available:
+        if(member_not_in_row["name"] in member_periods_prec) or (task_name in member_available["tasks"]):
+            members_available_not_in_row.remove(member_not_in_row)
+    
+    if(len (members_available_not_in_row) == 0):
+        return random.choice(members_available)
+    else:
+        return random.choice(members_available_not_in_row)
+    
+
 def assign_tasks():
     """ Assign tasks to the members """
-    # todo : Essayer qu’une personne ne fasse pas plusieurs tâches d’affilée
-    # todo : Essayer d’éviter un maximum qu’une personne fasse plusieurs fois la même tâche
     members = get_members()
     tuple_tasks_occurences = get_tasks_and_occurences()
     periods_info = tuple_tasks_occurences[1]
     tasks = tuple_tasks_occurences[0]
     attributed_tasks = []
-    nbTasks = 0
+    member_period_prec = []
 
     for period in periods_info:
         temp_task = []
         for task in tasks:
             if period in task["occurence"]:
                 temp_members = []
-
                 i = 0
                 while i < int(task["person"]):
-                    member = random.choice(members)
-                    while int(len(member["tasks"]) > nbTasks/int(len(members))):
-                        member = random.choice(members)
-
-                    nbTasks += 1
+                    member = choose_member(task["name"],period,members,member_period_prec)
                     members[members.index(member)]["tasks"].append(
                         task["name"])
+                    members[members.index(member)]["occurence"].append(
+                        period)
                     temp_members.append(member["name"])
                     i += 1
-
                 temp_task.append({
                     "name": task["name"],
                     "members": temp_members
@@ -86,7 +108,9 @@ def assign_tasks():
             "period": period,
             "tasks": temp_task
         })
+        member_period_prec = []
+        for person in temp_task:
+            member_period_prec.extend(person["members"])
     create_output(attributed_tasks)
-
 
 assign_tasks()
