cb3b2274777a4be25dc80b7200f026ff9cb0e94d
Maxime béver <max.bever98@gmail.com>
2020-04-21 22:48:07 +0200

    Add pdf

--- src/interface/function.js
+++ src/interface/function.js
@@ -1,6 +1,6 @@
 const path = require("path");
-const fs = require('fs')
-
+const fs = require("fs");
+const jsPDF = require("jspdf");
 const appRoot = path.resolve(__dirname);
 const projectPath = `${appRoot.split("\\interface")[0]}`;
 const exec = require("child_process").exec;
@@ -9,7 +9,7 @@ function openFile(file, message, fct) {
   exec(projectPath + file, (err, stdout, stderr) => {
     if (err) {
       alert(message);
-      console.log(err)
+      console.log(err);
       return;
     }
     fct();
@@ -17,7 +17,7 @@ function openFile(file, message, fct) {
 }
 
 function generateOutput() {
-  openFile(`\\script\\script.bat`, 'Cannot generate output', printOutput);
+  openFile(`\\script\\script.bat`, "Cannot generate output", printOutput);
 }
 
 function openMembersFile() {
@@ -25,24 +25,28 @@ function openMembersFile() {
 }
 
 function openTasksFile() {
-  openFile(`\\input\\task.txt`, 'Cannot open tasks file');
+  openFile(`\\input\\task.txt`, "Cannot open tasks file");
 }
 
-function printPDF(){
-  console.log('todo');
+function printPDF() {
+  let container = document.getElementById("outputContainer");
+  var doc = new jsPDF();
+
+  doc.fromHTML(container);
+  doc.save("membersAssignation.pdf");
 }
 
 function manageOutput() {
   fs.readFile(projectPath + `\\output\\output.json`, (err, data) => {
     if (err) {
-      console.log('Pas de fichier')
-      let container = document.getElementById('generateButtonContainer');
-      let button = document.createElement('button');
-      button.innerHTML = 'Generate output';
-      button.addEventListener('click', generateOutput);
-      button.id = 'generateOutputButton';
-      button.classList.add('btn');
-      button.classList.add('btn-danger');
+      console.log("Pas de fichier");
+      let container = document.getElementById("generateButtonContainer");
+      let button = document.createElement("button");
+      button.innerHTML = "Generate output";
+      button.addEventListener("click", generateOutput);
+      button.id = "generateOutputButton";
+      button.classList.add("btn");
+      button.classList.add("btn-danger");
       container.appendChild(button);
       return;
     } else {
@@ -52,18 +56,18 @@ function manageOutput() {
 }
 
 function printOutput() {
-  let container = document.getElementById('generateButtonContainer');
-  let button = document.getElementById('generateOutputButton');
+  let container = document.getElementById("generateButtonContainer");
+  let button = document.getElementById("generateOutputButton");
   if (button != undefined) container.removeChild(button);
 
   fs.readFile(projectPath + `\\output\\output.json`, (err, data) => {
     if (err) {
-      alert('Cannot open output file')
+      alert("Cannot open output file");
       return;
     }
     const attributed_task = JSON.parse(data);
 
-    let container = document.getElementById('outputContainer');
+    let container = document.getElementById("outputContainer");
 
     container.innerHTML = "";
 
@@ -75,35 +79,35 @@ function printOutput() {
     let memberElem;
 
     for (period of attributed_task) {
-      periodNameElem = document.createElement('h1');
+      periodNameElem = document.createElement("h1");
       periodNameElem.innerHTML = period.period;
-      periodNameElem.classList.add('periodName')
-      periodNameElem.classList.add('display-4')
+      periodNameElem.classList.add("periodName");
+      periodNameElem.classList.add("display-4");
 
       container.appendChild(periodNameElem);
 
-      taskListContainer = document.createElement('div');
-      taskListContainer.classList.add('taskListContainer');
+      taskListContainer = document.createElement("div");
+      taskListContainer.classList.add("taskListContainer");
 
       container.appendChild(taskListContainer);
 
       for (task of period.tasks) {
-        taskNameElem = document.createElement('h2');
+        taskNameElem = document.createElement("h2");
         taskNameElem.innerHTML = task.name;
-        taskNameElem.classList.add('taskName');
+        taskNameElem.classList.add("taskName");
 
-        taskContainer = document.createElement('div');
-        taskContainer.classList.add('taskContainer');
+        taskContainer = document.createElement("div");
+        taskContainer.classList.add("taskContainer");
 
         taskListContainer.appendChild(taskContainer);
 
         taskContainer.appendChild(taskNameElem);
 
-        listElem = document.createElement('ul');
+        listElem = document.createElement("ul");
 
         taskContainer.appendChild(listElem);
         for (member of task.members) {
-          memberElem = document.createElement('li');
+          memberElem = document.createElement("li");
           memberElem.innerHTML = member;
           listElem.appendChild(memberElem);
         }
